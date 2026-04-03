fbd5e86b95b718e6b3627c1a8078e01c8fa6f9ad
ZZIN772 <hyejinkwon772@gmail.com>
2026-04-03 11:12:12 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,35 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker installed");
+const CACHE_NAME = "booklist-v1";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+
+// 설치: 필요한 파일 캐시
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  console.log("Service Worker activated");
+// 활성화: 이전 캐시 삭제
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+// 요청: 캐시 우선, 없으면 네트워크
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => cached || fetch(event.request))
+    );
 });
